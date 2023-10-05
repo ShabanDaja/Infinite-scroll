08adbcf0f4704addda92cdb485d05888670692dc
Shaban Daja <shabandaja710@gmail.com>
2023-10-05 09:59:39 +0200

    Create Elements For Links & Photos, Add to DOM.

--- script.js
+++ script.js
@@ -8,6 +8,25 @@ const count = 10;
 const apiKey = "qgaFemBA2ib2b6yqn3NQphuuiu8sC22yA0BfdInkCBE";
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Create Elements For Links & Photos, Add to DOM
+function displayPhotos() {
+  //For Each method
+  photosArray.forEach((photo) => {
+    //Create <a> to link to Unsplash
+    const item = document.createElement("a");
+    item.setAttribute("href", photo.links.html);
+    item.setAttribute("target", "_blank");
+    // Create <img> for pohoto
+    const img = document.createElement("img");
+    img.setAttribute("src", photo.urls.regular);
+    img.setAttribute("alt", photo.alt_description);
+    img.setAttribute("title", photo.alt_description);
+    // Put <img> inside <a>, then put both inside imageContainer Element
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
 // get photos from unspalsh API
 async function getPhotos() {
   try {
